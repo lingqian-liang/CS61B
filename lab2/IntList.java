b9124a7594b4ae259d1dc3537a7be6f98691af70
lingqian-liang <32099641+lingqian-liang@users.noreply.github.com>
2020-12-15 22:46:41 -0500

    Create IntList.java

---
+++ lab2/IntList.java
@@ -0,0 +1,111 @@
+import java.util.Formatter;
+
+/**
+ * A naked recursive list of integers, similar to what we saw in lecture 3, but
+ * with a large number of additional methods.
+ *
+ * @author P. N. Hilfinger, with some modifications by Josh Hug and melaniecebula
+ *         [Do not modify this file.]
+ */
+public class IntList {
+    /**
+     * First element of list.
+     */
+    public int first;
+    /**
+     * Remaining elements of list.
+     */
+    public IntList rest;
+
+    /**
+     * A List with first FIRST0 and rest REST0.
+     */
+    public IntList(int first0, IntList rest0) {
+        first = first0;
+        rest = rest0;
+    }
+
+    /**
+     * A List with null rest, and first = 0.
+     */
+    public IntList() {
+    /* NOTE: public IntList () { }  would also work. */
+        this(0, null);
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Destructive.
+     */
+    public static void dSquareList(IntList L) {
+
+        while (L != null) {
+            L.first = L.first * L.first;
+            L = L.rest;
+        }
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Non-destructive.
+     */
+    public static IntList squareListIterative(IntList L) {
+        if (L == null) {
+            return null;
+        }
+        IntList res = new IntList(L.first * L.first, null);
+        IntList ptr = res;
+        L = L.rest;
+        while (L != null) {
+            ptr.rest = new IntList(L.first * L.first, null);
+            L = L.rest;
+            ptr = ptr.rest;
+        }
+        return res;
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Non-destructive.
+     */
+    public static IntList squareListRecursive(IntList L) {
+        if (L == null) {
+            return null;
+        }
+        return new IntList(L.first * L.first, squareListRecursive(L.rest));
+    }
+
+    /** DO NOT MODIFY ANYTHING ABOVE THIS LINE! */
+
+
+    /**
+     * Returns a list consisting of the elements of A followed by the
+     * *  elements of B.  May modify items of A. Don't use 'new'.
+     */
+
+    public static IntList dcatenate(IntList A, IntList B) {
+        //TODO:  fill in method
+        IntList A_copy = A;
+        while(A_copy.rest!=null){
+            A_copy = A_copy.rest;
+        }
+        A_copy.rest=B;
+        return A;
+    }
+
+    /**
+     * Returns a list consisting of the elements of A followed by the
+     * * elements of B.  May NOT modify items of A.  Use 'new'.
+     */
+    public static IntList catenate(IntList A, IntList B) {
+        //TODO:  fill in method
+        if(A == null){
+            if(B == null){
+                return null;
+            }
+            else{
+                return new IntList(B.first, catenate(B.rest,null));}
+        }
+        else{
+            return new IntList(A.first,catenate(A.rest,B));
+        }
+    }
+
+
